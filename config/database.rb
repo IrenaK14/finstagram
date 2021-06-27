5616bcded6ca3a3a694f91e964f543ff5601b4bd
IrenaK14 <ir.klancir@gmail.com>
2021-06-27 21:24:01 +0000

    Database.rb fix

--- config/database.rb
+++ config/database.rb
@@ -4,13 +4,25 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+  if Sinatra::Application.development?
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    db_url = "postgres://epajxkchcigwga:f65ac9e4c2b7e006444cf6de195670c54d30042cb459bb0089c779beaa153a65@ec2-3-214-136-47.compute-1.amazonaws.com:5432/dea33ac6a87pvo"
+    db = URI.parse(ENV['DATABASE_URL'] || db_url)
+    set :database, {
+      adapter: "postgresql",
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
 
-  # Load all models from app/models, using autoload instead of require
-  postgres://epajxkchcigwga:f65ac9e4c2b7e006444cf6de195670c54d30042cb459bb0089c779beaa153a65@ec2-3-214-136-47.compute-1.amazonaws.com:5432/dea33ac6a87pvo
+  # Load all models from app/models, using autoload instead of requirete
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
